432d3f212bd45edd8f0905e7168cda421d27c9a2
p1 <233@qq.com>
2016-06-22 12:50:59 +0800

    布局教学点模块的mapView控件,使其横竖屏都适合.

--- shijia/shijia/classes/教学点/controller/GDWMapController.m
+++ shijia/shijia/classes/教学点/controller/GDWMapController.m
@@ -13,6 +13,8 @@
 #import "GDWAnnotation.h"
 #import "GDWAnnotationViewCalloutView.h"
 
+#import <Masonry.h>
+
 @interface GDWMapController ()<MKMapViewDelegate>
 
 /** 地图 */
@@ -21,6 +23,9 @@
 @property (nonatomic, strong) NSMutableArray *annotations;
 /** 位置管理者*/
 @property (nonatomic, strong) CLLocationManager *locationM;
+
+/** 地理编码*/
+@property (nonatomic, strong) CLGeocoder *geoC;
 
 @end
 
@@ -40,6 +45,14 @@
     }
     return _locationM;
 }
+-(CLGeocoder *)geoC
+{
+    if (!_geoC) {
+        _geoC = [[CLGeocoder alloc] init];
+    }
+    return _geoC;
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -47,25 +60,87 @@
     //1.创建地图控件
     MKMapView *mapView = [[MKMapView  alloc]  init];
     self.mapView = mapView;
-    mapView.frame = self.view.bounds;
+    //mapView.frame = self.view.bounds;
     [self.view  addSubview:mapView];
     //设置代理.
     self.mapView.delegate = self;
     
+    /*
+     给mapVeiw设置约束
+     You have three choices when it comes to programmatically creating constraints:
+         1.You can use layout anchors,
+         2.you can use the NSLayoutConstraint class, or
+         3.you can use the Visual Format Language.
+     */
+    //使用Masonry第三方框架布局
+  
+    self.mapView.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    [self.mapView  mas_makeConstraints:^(MASConstraintMaker *make) {
+        
+        make.edges.equalTo(self.view).insets(UIEdgeInsetsMake(0, 0, 0, 0));
+    }];
+    
+    
     //2. 请求用户定位授权
     [self locationM];
+    
+    //3. 设置mapView
     // 在地图上,显示用户位置,(通过一个发光的蓝点(大头针))
-    // 地图不会跟着用户位置的变化而变化(不会自动放大地图)
     self.mapView.showsUserLocation = YES;
     // 给地图设置区域
-    //广州位于东经113°17'，北纬23°8'
-//    NSLog(@"%.2f",self.mapView.userLocation.coordinate.latitude);
-//    MKCoordinateSpan span = MKCoordinateSpanMake(0.301766, 0.222131);
-//    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(23.1, 113.1);
-//    MKCoordinateRegion region = MKCoordinateRegionMake(coordinate, span);
-//    [self.mapView setRegion:region animated:YES];
+    [self  setUpMapViewRegion];
+    
+    
 
 }
+//使用VFL布局的代码
+- (void)VFL:(UIView *)mapView{
+
+    if(floor(NSFoundationVersionNumber) > NSFoundationVersionNumber_iOS_6_1)
+    {
+        self.edgesForExtendedLayout = UIRectEdgeNone;
+    }
+    //将自适应向布局约束的转化关掉(根据情况有时需要有时不需要)
+    self.mapView.preservesSuperviewLayoutMargins = YES;
+    self.mapView.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    //创建一个存放约束的数组
+    NSMutableArray * tempConstraints = [NSMutableArray array];
+    
+    /*
+     创建水平方向的约束:在水平方向,leftButton距离父视图左侧的距离为80，leftButton宽度为60,rightButton和leftButton之间的距离为30，rightButton宽60
+     */
+    [tempConstraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[mapView]-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(mapView)]];
+    /*
+     创建竖直方向的约束:在竖直方向上,leftButton距离父视图顶部30，leftButton高度30
+     */
+    [tempConstraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-[mapView]-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(mapView)]];
+    //给视图添加约束
+    [self.view addConstraints:tempConstraints];
+}
+
+- (void)setUpMapViewRegion{//制定一个地点设置地图的区域.
+
+    NSString *address = @"广州市天河体育中心";
+    [self.geoC  geocodeAddressString:address completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+        
+        //1.获取位置标记
+        CLPlacemark *placemark = [placemarks firstObject];
+    
+        //2.设置区域
+        MKCoordinateSpan span = MKCoordinateSpanMake(0.301766, 0.222131);
+        CLLocationCoordinate2D coordinate = placemark.location.coordinate;
+        MKCoordinateRegion region = MKCoordinateRegionMake(coordinate, span);
+        [self.mapView setRegion:region animated:YES];
+        
+    }];
+
+
+
+}
+
+
 
 - (void)viewWillAppear:(BOOL)animated{
 
@@ -117,25 +192,22 @@
  *  当地图更新到用户位置时调用
    self.mapView.userLocation.coordinate属性值,好像只能在代理方法中拿到值.
  */
--(void)mapView:(nonnull MKMapView *)mapView didUpdateUserLocation:(nonnull MKUserLocation *)userLocation
-{
-    /**
-     *  MKUserLocation : 大头针数据模型,  只不过它遵循了大头针数据模型必须遵循的一个协议 MKAnnotation
-     */
-    
-    // 只是设置地图的中心, 不会放大地图
-    // [mapView setCenterCoordinate:userLocation.location.coordinate animated:YES];
-    
-    // 不但要显示地图中心, 还要缩放地图到合适的比例
-    // 创建一个区域跨度
-    MKCoordinateSpan span = MKCoordinateSpanMake(0.301766, 0.222131);
-    // 根据一个中心和跨度, 创建一个区域
-    MKCoordinateRegion region = MKCoordinateRegionMake(userLocation.location.coordinate, span);
-    // 设置地图区域
-    [mapView setRegion:region animated:YES];
-    
-    
-}
+//-(void)mapView:(nonnull MKMapView *)mapView didUpdateUserLocation:(nonnull MKUserLocation *)userLocation
+//{
+//    /**
+//     *  MKUserLocation : 大头针数据模型,  只不过它遵循了大头针数据模型必须遵循的一个协议 MKAnnotation
+//     */
+//    
+//    // 不但要显示地图中心, 还要缩放地图到合适的比例
+//    // 创建一个区域跨度
+//    MKCoordinateSpan span = MKCoordinateSpanMake(0.301766, 0.222131);
+//    // 根据一个中心和跨度, 创建一个区域
+//    MKCoordinateRegion region = MKCoordinateRegionMake(userLocation.location.coordinate, span);
+//    // 设置地图区域
+//    [mapView setRegion:region animated:YES];
+//    
+//    
+//}
 
 
 /**
